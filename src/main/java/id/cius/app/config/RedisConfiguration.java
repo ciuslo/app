55b17d1896ce54bdab4a0b74619e69e69f0ab759
ciuslo <50345887+ciuslo@users.noreply.github.com>
2022-06-22 14:24:56 +0700

    redis update

--- src/main/java/id/cius/app/config/RedisConfiguration.java
+++ src/main/java/id/cius/app/config/RedisConfiguration.java
@@ -4,12 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 
 import id.cius.app.model.Actor;
+import id.cius.app.service.ActorBackgroundService;
 
 @Configuration
 public class RedisConfiguration {
-    
+
     @Bean
     RedisTemplate<Integer, Actor> actorRedisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<Integer, Actor> template = new RedisTemplate<>();
@@ -17,8 +19,8 @@ public class RedisConfiguration {
         return template;
     }
 
-   
-    
-    
+    MessageListenerAdapter messageListener() {
+        return new MessageListenerAdapter(new ActorBackgroundService());
+    }
 
 }

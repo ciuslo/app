32838d8a3c51762309e416b214d458afb316eb23
ciuslo <50345887+ciuslo@users.noreply.github.com>
2022-06-23 14:50:08 +0700

    return to array

--- src/main/java/id/cius/app/service/ActorQueryService.java
+++ src/main/java/id/cius/app/service/ActorQueryService.java
@@ -1,11 +1,15 @@
 package id.cius.app.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import com.google.gson.Gson;
 
 import id.cius.app.model.Actor;
 import id.cius.app.repository.ActorRepository;
@@ -16,6 +20,9 @@ public class ActorQueryService {
 
     @Autowired
     private ActorRepository  actorRepository;
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
 
     public List<Actor> getAll(){
         List<Actor> actors = actorRepository.findAll();
@@ -30,6 +37,17 @@ public class ActorQueryService {
 
         return Stream.concat(actorsFirstName.stream(), actorsLastName.stream())
         .collect(Collectors.toList());
+
+    }
+
+    public List<Actor> searchRedis(String keyword){
+        List<Actor> a = new ArrayList<>();
+        List<String> r = new ArrayList<>(redisTemplate.opsForSet().members("name_"+keyword.substring(0, 2)));
+        for (String string : r) {
+            Actor ac = new Gson().fromJson(string, Actor.class);
+            a.add(ac);
+        }
+        return a;
 
     }
 
